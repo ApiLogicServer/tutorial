0f2ce5f04d4de2066fe5f535f8c1f872253278ca
Val Huber <valjhuber@gmail.com>
2023-04-09 07:58:41 -0700

    flexible dev & deploy

--- 3. Logic/api_logic_server_run.py
+++ 3. Logic/api_logic_server_run.py
@@ -3,7 +3,7 @@
 """
 ==============================================================================
 
-    This file initializes and starts the API Logic Server (v 08.01.24, April 08, 2023 18:10:44):
+    This file initializes and starts the API Logic Server (v 08.01.24, April 09, 2023 07:56:57):
         $ python3 api_logic_server_run.py [--help]
 
     Then, access the Admin App and API via the Browser, eg:  
@@ -87,7 +87,7 @@ for each_arg in sys.argv:
         args += ", "
 project_name = os.path.basename(os.path.normpath(current_path))
 app_logger.info(f'\nAPI Logic Project ({project_name}) Starting with args: \n.. {args}\n')
-app_logger.info(f'Created April 08, 2023 18:10:44 at {str(current_path)}\n')
+app_logger.info(f'Created April 09, 2023 07:56:57 at {str(current_path)}\n')
 
 from typing import TypedDict
 import safrs  # fails without venv - see https://apilogicserver.github.io/Docs/Project-Env/
